1eb090905e83b2b567556b847a6257691ed7f18e
kitsudo1412 <kitsudo1412@gmail.com>
2016-02-23 14:19:30 +0700

    Add initial files for shopquanao ('the theme'), add bootstrap css for responsive purpose

--- wp-content/themes/shopquanao/header.php
+++ wp-content/themes/shopquanao/header.php
@@ -2,11 +2,9 @@
 /**
  * The Header template for our theme
  *
- * Displays all of the <head> section and everything up till <div id="main">
- *
- * @package WordPress
- * @subpackage Twenty_Twelve
- * @since Twenty Twelve 1.0
+ * @package Niteco
+ * @subpackage shopquanao
+ * @since shopquanao 1.0
  */
 ?><!DOCTYPE html>
 <!--[if IE 7]>
@@ -20,7 +18,7 @@
 <!--<![endif]-->
 <head>
 <meta charset="<?php bloginfo( 'charset' ); ?>" />
-<meta name="viewport" content="width=device-width" />
+<meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no" />
 <title><?php wp_title( '|', true, 'right' ); ?></title>
 <link rel="profile" href="http://gmpg.org/xfn/11" />
 <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
@@ -48,6 +46,10 @@
 		<?php if ( get_header_image() ) : ?>
 		<a href="<?php echo esc_url( home_url( '/' ) ); ?>"><img src="<?php header_image(); ?>" class="header-image" width="<?php echo esc_attr( get_custom_header()->width ); ?>" height="<?php echo esc_attr( get_custom_header()->height ); ?>" alt="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" /></a>
 		<?php endif; ?>
-	</header><!-- #masthead -->
+	</header>
+	
+	<div id="header">
+	
+	</div>
 
 	<div id="main" class="wrapper">
--- wp-content/themes/shopquanao/index.php
+++ wp-content/themes/shopquanao/index.php
@@ -1,17 +1,9 @@
 <?php
 /**
  * The main template file
- *
- * This is the most generic template file in a WordPress theme
- * and one of the two required files for a theme (the other being style.css).
- * It is used to display a page when nothing more specific matches a query.
- * For example, it puts together the home page when no home.php file exists.
- *
- * @link https://codex.wordpress.org/Template_Hierarchy
- *
- * @package WordPress
- * @subpackage Twenty_Twelve
- * @since Twenty Twelve 1.0
+ * @package Niteco
+ * @subpackage shopquanao
+ * @since shopquanao 1.0
  */
 
 get_header(); ?>
